08e0b9cbf74b7a2f28221e61bd8525c58f9e0524
Owen Wexler <owenbwexler@gmail.com>
2020-11-23 19:55:48 -0500

    Fix logic errors in Stack class

--- stack_queue_list.py
+++ stack_queue_list.py
@@ -7,8 +7,22 @@ class Stack:
     return
 
   def pop(self):
-    return self._data.pop()
+    removed_item = self._data[0]
+    self._data.remove(self._data[0])
+    return removed_item
 
   def length(self):
     return len(self._data)
 
+  def log(self):
+    return self._data
+
+# test_stack = Stack()
+# test_stack.push(4)
+# test_stack.push(3)
+# test_stack.push(2)
+# test_stack.push(1)
+# print("current stack: ", test_stack.log())
+# popped_item = test_stack.pop()
+# print("popped_item: ", popped_item)
+# print("current stack: ", test_stack.log())
